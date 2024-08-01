9b94e11d8b07e4c5bfc2cd2d0d759b67be0a5803
Anastasiya Niadbailik <anastasiya.niadbailik@gmail.com>
2024-08-01 12:29:42 +0300

    edit test

--- pages/register_page.js
+++ pages/register_page.js
@@ -1,7 +1,8 @@
+const timeOut = 300;
 exports.RegisterPage = class RegisterPage{
     constructor(page) {
         this.page = page;
-        this.sign_up_supplier = page.locator('//*[@id="root"]/div/div/div/form/div[1]/button[2]')
+        this.sign_up_supplier = page.locator('//*[@id="root"]/div/div/div/form/div[1]/button[2]');
         this.email_field = page.locator('//*[@id="root"]/div/div/div/form/div[2]/input');
         this.password_field = page.locator('//*[@id="root"]/div/div/div/form/div[3]/input');
         this.create_account_btn = page.locator('//*[@id="root"]/div/div/div/form/button');
@@ -11,28 +12,33 @@ exports.RegisterPage = class RegisterPage{
     async open_registration_page() {
         this.page.goto('https://dev.abra-market.com/register');
         // Ожидание, пока страница полностью загрузится
-        await page.waitForLoadState('load');
+        await this.page.waitForLoadState('load');
         // await this.page.waitForTimeout(timeOut);
         // await page.getByText("I'm here to sell").toBeVisible();
+        // await this.page.waitForSelector('#root > div > div > div > form > div.RegisterForm_buttons_container_user_role__odHqW > button.Button_button__-\+nph.Button_black__dV9Cl.RegisterForm_button_user_role__ax6bX');
     }
 
-    async sign_up_supplier() {
+    async click_sign_up_supplier() {
         this.sign_up_supplier.click();
+        await this.page.waitForTimeout(timeOut);
     }
 
     async fill_email() {
         this.email_field.focus();
-        this.email_field.fill('anast@gmail.com');
+        this.email_field.fill('anarsqqwetasrtiaya@gmail.com');
+        await this.page.waitForTimeout(timeOut);
     }
 
     async fil_email_with_invalid_email() {
         this.login_field.focus();
         this.login_field.fill('testgmail.com');
+        await this.page.waitForTimeout(timeOut);
     }
 
     async fill_password() {
         this.password_field.focus();
         this.password_field.fill('Bobo123456!');
+        await this.page.waitForTimeout(timeOut);
     }
     
     async fill_password_invalid() {
@@ -42,6 +48,7 @@ exports.RegisterPage = class RegisterPage{
 
     async create_supplier_account() {
         this.create_account_btn.click();
+        await this.page.waitForTimeout(500);
     }
 
 }
--- tests/register.test.js
+++ tests/register.test.js
@@ -5,10 +5,11 @@ test('registration_positive', async ({page}) => {
     const Register = new RegisterPage(page);
 
     await Register.open_registration_page();
-    await Register.sign_up_supplier();
+    await Register.click_sign_up_supplier();
     await Register.fill_email();
     await Register.fill_password();
     await Register.create_supplier_account();
-    await expect(page).toHaveURL('/.*check_email/');
+    // await expect(page).toHaveURL('/check_email$/');
+    await expect(page).toHaveURL('https://dev.abra-market.com/register/check_email');
     // await expect(page).getByText('A link for sign up has been sent to your email address.').toBeVisible();
 })
